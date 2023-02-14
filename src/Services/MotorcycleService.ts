75aca6fc10dc056fa3870fb87d7f914bbef3be50
Carina Cunha <carina.cunha13@gmail.com>
2023-02-14 15:04:47 -0300

    feat: Correct error linter

--- src/Services/MotorcycleService.ts
+++ src/Services/MotorcycleService.ts
@@ -1,18 +1,17 @@
 import Motorcycles from '../Domains/Motorcycle';
 import IMotorcycle from '../Interfaces/IMotorcycle';
-import IMotorcycles from '../Interfaces/IMotorcycle';
 import MotorcyclesODM from '../Models/motorclyclesODM';
 
 export default class MotorcyclesService {
   constructor(private _model = new MotorcyclesODM()) {}
-  public createMotorDomain(motor: IMotorcycles | null): Motorcycles | null {
+  public createMotorDomain(motor: IMotorcycles | null): Motorcycle | null {
     if (motor) {
       return new Motorcycles(motor);
     }
     return null;
   }
 
-  public async create(motor: IMotorcycles) {
+  public async create(motor: IMotorcycle) {
     const newMotor = await this._model.create(motor);
     return this.createMotorDomain(newMotor);
   }
